2c80a9e38d02c5759b8de1b47be63043977dea21
Fathima1219 <fathima1942004@gmail.com>
2026-07-29 20:49:46 +0530

    Create string_analyzer.py

---
+++ string_analyzer.py
@@ -0,0 +1,8 @@
+# String Manipulation and Name Analyzer Tool
+name = input("Enter your name: ")
+
+print("First letter:", name[0])
+print("Last 3 letters:", name[-3:])
+print("Uppercase:", name.upper())
+print("With Surname:", name + " Reddy")
+print(f"Length: {len(name)}")
